edbe5ef943eef87dc80d384233ac0e418f81f125
Rajarajacholan Krishnamurthy <Rajarajacholan.Krishnamurthy@BWDesignGroup.com>
2020-08-29 01:02:58 -0500

    base version

---
+++ GraphQL/GraphSchema.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL;
+using GraphQL.Types;
+
+namespace GraphQLDemo.GraphQL
+{
+    public class GraphSchema : Schema
+    {
+        public GraphSchema(IDependencyResolver resolver) :
+           base(resolver)
+        {
+            Query = resolver.Resolve<RootQuery>();
+        }
+    }
+
+}
